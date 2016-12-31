e7971e596f46cc9d49b113cbf6da04a21c26b949
sophatvathana <sovathana.phat@gmail.com>
2016-12-31 12:23:29 +0700

    use right ignore

--- IPCAM-VIDEO-STREAMING-API/src/main/java/kh/com/kshrd/ipcam/emun/ControlEnum.java
+++
@@ -1,8 +0,0 @@
-package kh.com.kshrd.ipcam.emun;
-
-/**
- * Created by sophatvathana on 21/12/16.
- */
-public enum ControlEnum {
-    left , right, up, down, stop, zoomin, zoomout
-}
--- IPCAM-VIDEO-STREAMING-API/src/main/java/kh/com/kshrd/ipcam/respone/ResponseMessage.java
+++
@@ -1,9 +0,0 @@
-package kh.com.kshrd.ipcam.respone;
-
-public interface ResponseMessage {
-	final String CAMERA_MESSAGE="CAMERA";
-	final String MODEL_MESSAGE="MODEL";
-	final String USER_MESSAGE="USER";
-	final String PLUGIN_MESSAGE="PLUGIN";
-	
-}
